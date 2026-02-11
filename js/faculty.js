22a93f748987e2fcc6f2bffad6ece488b32ed959
prawin1505 <prawinkumarsn05@gmail.com>
2026-02-11 09:34:41 +0530

    Faculty page

--- js/faculty.js
+++ js/faculty.js
@@ -1,36 +1,90 @@
 const faculty = {
     hod: {
-        img: "../Images/faculty/hod.jpg",
+        img: "../Images/faculty/H.LILLY BEAULAH.JPG",
         name: "Dr. H. Lilly Beaulah",
         role: "Professor & Head of Department",
-        spec: "Specialization: Machine Learning, NLP, Comput er Vision",
         email: "Email: hodcse@mahendacollege.com",
-        exp: "Experience: 30+ Years"
+        exp: "Experience: 39 Years"
+    },
+    jenolinrex: {
+        img: "../Images/faculty/M.JENOLIN REX.jpg",
+        name: "Mr. M. Jenolin Rex",
+        role: "Assistant Professor",
+        email: "Email: jenolinrex@mahendracollege.com",
+        exp: "Experience: 15 Years"
     },
     dhanakodi: {
-        img: "../Images/faculty/Dhanakodiv.jpg",
-        name: "Ms. Dhanakodi V",
+        img: "../Images/faculty/V.DHANAKODI.jpg",
+        name: "Ms. V. Dhanakodi",
         role: "Assistant Professor",
-        spec: "Specialization: Data Science & Big Data Analytics",
-        email: "Email: dhanakodiv@mahendacollege.com",
+        email: "Email: dhanakodiv@mahendracollege.com",
         exp: "Experience: 4 Years"
     },
     gayathiri: {
-        img: "../Images/faculty/gayathiri.jpg",
-        name: "Mrs. Gayathiri M",
+        img: "../Images/faculty/GAYATHRI.M.jpg",
+        name: "Mrs. M. Gayathiri",
         role: "Assistant Professor",
-        spec: "Specialization: Cyber Security",
         email: "Email: gayathirim@mahendracollege.com",
         exp: "Experience: 4 Years"
     },
-    sakthivel: {
-        img: "../Images/faculty/sakthivel.jpg",
-        name: "Mr. Sakthivel P",
-        role: "Professor",
-        spec: "Specialization: AI & Machine Learning",
-        email: "Email: sakthivelp@mahendracollege.com",
+    anandraj: {
+        img: "../Images/faculty/ANANDRAJ.M.jpg",
+        name: "Mr. M. Anandraj",
+        role: "Assistant Professor",
+        email: "Email: anandraj@mahendracollege.com",
+        exp: "Experience: 13 Years"
+    },
+    amjath: {
+        img: "../Images/faculty/AMJATH A.JPG",
+        name: "Mr. Amjath",
+        role: "Assistant Professor",
+        email: "Email: amjath@mahendracollege.com",
         exp: "Experience: 4 Years"
+    },
+    nishadevi: {
+        img: "../Images/faculty/NISHADEVI.V.jpg",
+        name: "Mrs. V. Nishadevi",
+        role: "Assistant Professor",
+        email: "Email: nishadevi@mahendracollege.com",
+        exp: "Experience: 13 Years"
+    },
+    indhuja: {
+        img: "../Images/faculty/A.INDHUJA.jpg",
+        name: "Mrs. A. Indhuja",
+        role: "Assistant Professor",
+        email: "Email: indhujaa@mahendracollege.com",
+        exp: "Experience: 8 Years"
+    },
+    deepa: {
+        img: "../Images/faculty/V.DEEPA.jpg",
+        name: "Mrs. V. Deepa",
+        role: "Assistant Professor",
+        email: "Email: deepav@mahendracollege.com",
+        exp: "Experience: 12 Years"
+    },
+    vinithasree: {
+        img: "../Images/faculty/L.VINITHASREE.jpg",
+        name: "Mrs. L. Vinithasree",
+        role: "Assistant Professor",
+        email: "Email: vinithasree@mahendracollege.com",
+        exp: "Experience: 8 Years"
+    },
+    sathya: {
+        img: "../Images/faculty/M.SATHYA.jpg",
+        name: "Mrs. M. Sathya",
+        role: "Assistant Professor",
+        email: "Email: sathya@mahendracollege.com",
+        exp: "Experience: 11 Years"
+    },
+    shanmugapriya: {
+        img: "../Images/faculty/M.SHANMUGAPRIYA.jpg",
+        name: "Mrs. M. Shanmugapriya",
+        role: "Assistant Professor",
+        email: "Email: shanmugapriya@mahendracollege.com",
+        exp: "Experience: 12 Years"
     }
+
+
 };
 
 function openFaculty(id) {
@@ -38,7 +92,6 @@ function openFaculty(id) {
     document.getElementById("m-img").src = f.img;
     document.getElementById("m-name").innerText = f.name;
     document.getElementById("m-role").innerText = f.role;
-    document.getElementById("m-spec").innerText = f.spec;
     document.getElementById("m-email").innerText = f.email;
     document.getElementById("m-exp").innerText = f.exp;
     document.getElementById("facultyModal").style.display = "flex";
